c56f72f9d66fa5915f8c455c765c48f725fc03b6
Monu Tanwar <dalip8159@gmail.com>
2026-04-14 12:02:45 +0530

    Added default values for env variables

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_CONFIG = {
-    "host":     os.environ.get("MYSQLHOST"),
-    "user":     os.environ.get("MYSQLUSER"),
-    "password": os.environ.get("MYSQLPASSWORD"),
-    "database": os.environ.get("MYSQLDATABASE"),
-    "port":     int(os.environ.get("MYSQLPORT")),
+    "host":     os.environ.get("MYSQLHOST",     "localhost"),
+    "user":     os.environ.get("MYSQLUSER",     "root"),
+    "password": os.environ.get("MYSQLPASSWORD", "Monu#000"),
+    "database": os.environ.get("MYSQLDATABASE", "naukri_jobs"),
+    "port":     int(os.environ.get("MYSQLPORT",  "3306")),  # string default so int() never gets None
 }
 
 SCRAPE_URL = "https://www.naukri.com/development-jobs"
